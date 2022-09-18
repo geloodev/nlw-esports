52f816419b89c76d2bca23a13c31fb17389ca674
geloodev <angeloholandini11@gmail.com>
2022-09-18 12:03:18 -0300

    server update

---
+++ server/build/server.js
@@ -0,0 +1,96 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const client_1 = require("@prisma/client");
+const convert_hour_string_to_minutes_1 = require("./utils/convert-hour-string-to-minutes");
+const convert_minutes_to_hour_string_1 = require("./utils/convert-minutes-to-hour-string");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+//COSR allowed origins config
+const allowedOrigins = ['http://localhost:8080'];
+const options = {
+    origin: allowedOrigins
+};
+app.use((0, cors_1.default)(options));
+const prisma = new client_1.PrismaClient({
+    log: ['query'] //show all sql logs
+});
+app.get('/games', (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    const games = yield prisma.game.findMany({
+        // include the ads of the games
+        include: {
+            _count: {
+                select: {
+                    Ads: true,
+                }
+            }
+        }
+    });
+    return response.json([games]);
+}));
+app.post('/games/:id/ads', (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    const gameId = request.params.id;
+    const body = request.body;
+    const ad = yield prisma.ad.create({
+        data: {
+            gameId,
+            name: body.name,
+            yearsPlaying: body.yearsPlaying,
+            discord: body.discord,
+            weekDays: body.weekDays.join(','),
+            hourStart: (0, convert_hour_string_to_minutes_1.convertHourStringToMinutes)(body.hourStart),
+            hourEnd: (0, convert_hour_string_to_minutes_1.convertHourStringToMinutes)(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel,
+        }
+    });
+    return response.status(201).json(ad); //201 is a HTTP code
+}));
+app.get('/games/:id/ads', (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    const gameId = request.params.id;
+    const ads = yield prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true,
+        },
+        where: {
+            gameId,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        }
+    });
+    return response.json(ads.map(ad => {
+        return Object.assign(Object.assign({}, ad), { weekDays: ad.weekDays.split(','), hourStart: (0, convert_minutes_to_hour_string_1.convertMinutesToHourString)(ad.hourStart), hourEnd: (0, convert_minutes_to_hour_string_1.convertMinutesToHourString)(ad.hourEnd) });
+    }));
+}));
+app.get('/ads/:id/discord', (request, response) => __awaiter(void 0, void 0, void 0, function* () {
+    const adId = request.params.id;
+    const ad = yield prisma.ad.findUniqueOrThrow({
+        select: {
+            discord: true,
+        },
+        where: {
+            id: adId,
+        }
+    });
+    return response.json(ad);
+}));
+app.listen(8080);
